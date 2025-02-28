a4118ec612ebb3eb9e72110cc0a9013ebeb9d198
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2025-02-28 14:08:37 -0500

    feat: manifest and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../style/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import Script from 'next/script';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
@@ -26,6 +27,12 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 		);
 	return (
 		<ChakraProvider theme={theme}>
+			<Head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+				<link rel="manifest" href="/site.webmanifest"/>
+			</Head>
 			<Script
 				defer
 				src="https://cloud.umami.is/script.js"
