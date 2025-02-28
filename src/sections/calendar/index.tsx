0fcf1a27e2091025668a0f0ea28769e018229f41
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2025-02-28 15:39:51 -0500

    feat: add tipping notice

--- src/sections/calendar/index.tsx
+++ src/sections/calendar/index.tsx
@@ -2,6 +2,7 @@ import {
 	Box,
 	Heading,
 	Flex,
+	Text,
 } from '@chakra-ui/react';
 import { EventInfo } from '@util/importDateFiles';
 import MonthTabs from './components/MonthTabs';
@@ -9,27 +10,46 @@ import MonthTabs from './components/MonthTabs';
 function Calendar({ dates }: { dates: EventInfo[] }) {
 	const currentYear = new Date().getFullYear();
 	return (
-		<Flex
-			id='dates'
+		<Box
 			backgroundColor='brand.lightblue'
 			py={16}
-			px={{ base: 4, md: 8 }}
-			justifyContent='center'
+			id='dates'
 		>
-			<Box
-				w='full'
-				maxW='container.xl'
-				flexDir={{ base: 'column', md: 'row-reverse' }}
+			<Flex
+				px={{ base: 4, md: 8 }}
+				justifyContent='center'
 			>
-				<Heading
-					size='3xl'
-					textAlign='center'
+				<Box
+					w='full'
+					maxW='container.xl'
+					flexDir={{ base: 'column', md: 'row-reverse' }}
 				>
-					{currentYear} Calendar
-				</Heading>
-				<MonthTabs dates={dates}/>
-			</Box>
-		</Flex>
+					<Heading
+						size='3xl'
+						textAlign='center'
+					>
+						{currentYear} Calendar
+					</Heading>
+					<MonthTabs dates={dates}/>
+				</Box>
+			</Flex>
+			<Flex
+				justifyContent='center'
+				px={{ base: 4, md: 8 }}
+			>
+				<Box
+					my={8}
+					borderRadius={12}
+					backgroundColor='brand.yellow'
+					p={8}
+					maxW='50em'
+				>
+					<Text>
+						<b>NOTE:</b> Our artists are volunteering their time and skills to put on this amazing event. If you can, please plan on tipping the performers via cash or Venmo. Your support is much appreciated!
+					</Text>
+				</Box>
+			</Flex>
+		</Box>
 	);
 }
 
